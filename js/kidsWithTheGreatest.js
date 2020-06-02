ec129ce3619615b2f22f1485da64f81eff0f40e0
Finesse Carter <shop.scrxvms@gmail.com>
2020-06-02 13:04:33 -0400

    new leet code

--- js/kidsWithTheGreatest.js
+++ js/kidsWithTheGreatest.js
@@ -1,6 +1,7 @@
 /**Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
 
-For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
+For each kid check if there is a way to distribute extraCandies among the kids such that he
+ or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
 
  
 
@@ -39,7 +40,9 @@ Constraints:
 var kidsWithCandies = function(candies, extraCandies) {
     console.log(arguments )
     let maxNumber = 0;
+    //sum
     let result =[];
+    //bigsum
 // witch kid has the most kandy    
 
 for(let i=0; i<candies.length;i++){
